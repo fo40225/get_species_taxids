9cdb8c8045dd589e8f7c3a3c1df9b5e0dfcc5fce
fo40225 <fo40225@hotmail.com>
2022-11-10 11:45:58 +0800

    merged id handle

--- get_species_taxids.py
+++ get_species_taxids.py
@@ -1,6 +1,8 @@
 import argparse
 import pickle
-from anytree import Walker, LevelOrderIter
+import sys
+
+from anytree import LevelOrderIter, Walker
 
 
 def main():
@@ -8,6 +10,7 @@ def main():
     parser.add_argument("-t", help=" a file contains taxonomy ID pre line will get taxonomy IDs at specific taxonomy rank level ")
     parser.add_argument("-l", "--level", help=" ncbi taxonomy rank level ", default='species', required=False)
     parser.add_argument("--tree", help=" prebuild tree path ", default='nodes.pkl', required=False)
+    parser.add_argument("--merged", help=" merged.tmp ", default='merged.dmp', required=False)
     args = parser.parse_args()
 
     with open(args.tree, 'rb') as f:
@@ -16,11 +19,28 @@ def main():
     root = nodes['1']
     w = Walker()
     results = set()
+    dict_merged = {}
 
     with open(args.t) as t:
         for line in t.readlines():
             taxid = line.strip('\n')
-            node = nodes[taxid]
+
+            node = nodes.get(taxid)
+            if node is None:
+                if len(dict_merged) == 0:
+                    with open(args.merged) as f:
+                        for l in f.readlines():
+                            cells = l.split('|')
+                            old_id = cells[0].strip()
+                            new_id = cells[1].strip()
+                            dict_merged[old_id] = new_id
+
+                new_taxid = dict_merged.get(taxid)
+                node = nodes.get(new_taxid)
+
+                if node is None:
+                    print(f"taxid not found: {taxid}", file=sys.stderr)
+                    continue
 
             if node.rank == args.level:
                 results.add(node.tax_id)
